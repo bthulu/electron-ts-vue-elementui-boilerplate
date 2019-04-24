0f116b3b25071eab73ea7a9dfccf6f35678794b3
gejian <gosen2174>
2019-04-24 22:43:46 +0800

    '增加右键菜单:检查元素'

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, Menu} from "electron";
 
 function createWindow() {
-    let win = new BrowserWindow({width: 1024, height: 700});
+    let win = new BrowserWindow({show: false, width: 1024, height: 700});
     win.loadURL(`file://${__dirname}/index.html`);
     if (process.env.NODE_ENV === 'development') {
         require('devtron').install();
         require('electron-debug')();
         win.webContents.openDevTools();
+        win.webContents.on('context-menu', (e, props) => {
+            const {x, y} = props;
+            Menu.buildFromTemplate([{
+                label: 'Inspect element',
+                click: () => win.webContents.inspectElement(x, y)
+            }]).popup({
+                window: win
+            });
+        });
+        win.once('ready-to-show', () => {
+            win.show();
+        });
     }
 }
 
